3044e4b150a8d7f63702683c0410e4fc21679dd1
Shea Newton <shnewto@gmail.com>
2020-07-16 08:31:15 -0600

    seem to be parsing and lived to talk about it

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,87 @@
 extern crate hex;
 extern crate pest;
+extern crate radix_fmt;
 #[macro_use]
 extern crate pest_derive;
 
+use hex::FromHex;
 use pest::Parser;
-use std::fs;
-use std::fs::File;
-use std::io::Read;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 pub struct BinaryParser;
 
+type Mp3Parse<'a> = pest::iterators::Pair<'a, Rule>;
+
 fn main() -> std::io::Result<()> {
     let fname = "res/ghohor017_15_owlcreekbridge_bierce_lems_64kb.mp3";
-    // an occurance at owl creek bridge
-    // let mut file = File::open("res/ghohor017_15_owlcreekbridge_bierce_lems_64kb.mp3")?;
-
-    // ```
-    // $ wc -c res/ghohor017_15_owlcreekbridge_bierce_lems_64kb.mp3
-    // 12289024
-    // ```
-    //let mut buf = [0u8; 12289024];
-
-    //let bytes_read = file.read(&mut buf)?;
-
-    // let mut buffer = String::new();
-    // let mut f = File::open(fname)?;
-    // f.read_to_string(&mut buffer)?;
-
     let buffer = std::fs::read(fname)?;
     let hexbuffer = hex::encode(&buffer);
 
-    //println!("{:?}", buffer);
+    // maybe something like this if you want all base 2 data to parse...?
+    let _base2buffer = buffer
+        .into_iter()
+        .map(|n| format!("{}", radix_fmt::radix(n, 2)))
+        .collect::<Vec<String>>();
 
-    let ret = BinaryParser::parse(Rule::mp3_header, &hexbuffer[0..3]).unwrap();
+    let mut m: usize = 0;
+    let mut n: usize = 3;
+    let end: usize = hexbuffer.len() - 1;
 
-    println!("{:?}", ret);
+    let header: Mp3Parse = BinaryParser::parse(Rule::mp3_header, &hexbuffer[m..n])
+        .unwrap()
+        .next()
+        .unwrap();
+
+    println!("header: {:?}", header);
+
+    m = 4;
+    n = 8;
+    while n < end {
+        let mut step = 4;
+
+        let parsed_data = BinaryParser::parse(Rule::mp3_hex_content, &hexbuffer[m..n])
+            .unwrap()
+            .next();
+
+        if let Some(value) = parsed_data {
+            match value.as_rule() {
+                Rule::important_hex_shape_a => {
+                    if let Ok(v) = Vec::from_hex(value.as_str()) {
+                        if let Some(last) = v.last() {
+                            step = *last as usize
+                        }
+                    } else {
+                        step = 8;
+                    }
+                    println! {"got importnat shape a: {:?}", value.as_str()};
+                }
+
+                Rule::important_hex_shape_b => {
+                    if let Ok(v) = Vec::from_hex(value.as_str()) {
+                        if let Some(first) = v.first() {
+                            step = *first as usize
+                        }
+                    } else {
+                        step = 10;
+                    }
+                    println! {"got importnat shape b: {:?}", value.as_str()};
+                }
+
+                Rule::hex_byte => {
+                    // look for something else...?
+                }
+
+                _ => {
+                    // handle the other things
+                    break;
+                }
+            }
+        }
+
+        m = n;
+        n = n + step;
+    }
+
     Ok(())
 }
